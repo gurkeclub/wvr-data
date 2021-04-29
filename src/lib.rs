0b24a43624f3ab22963437101c5d44f638f604d0
champyrti <champi@gurke.club>
2021-04-29 15:19:39 +0200

    Add pause and play functions to InputProviders

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ extern crate serde_derive;
 
 use std::path::{Path, PathBuf};
 
+use anyhow::Result;
+
 use directories_next::ProjectDirs;
 
 pub mod config;
@@ -58,7 +60,15 @@ pub trait InputProvider {
     fn set_property(&mut self, property: &str, value: &DataHolder);
     fn set_beat(&mut self, _bpm: f64, _sync: bool) {}
     fn set_time(&mut self, _time: f64, _sync: bool) {}
-    fn stop(&mut self) {}
+    fn stop(&mut self) -> Result<()> {
+        Ok(())
+    }
+    fn pause(&mut self) -> Result<()> {
+        Ok(())
+    }
+    fn play(&mut self) -> Result<()> {
+        Ok(())
+    }
 }
 
 pub fn get_data_path() -> PathBuf {
